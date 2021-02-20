91da5164b724219456374e68ddbc6dd2ac14bc9a
Anthony Gatturna <agatturn@student.fitchburgstate.edu>
2021-02-20 13:28:11 -0500

    Changes to Unity Collision

---
+++ Mobile Game 1/2D_MobileGame1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    public GameObject deathEffect;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
